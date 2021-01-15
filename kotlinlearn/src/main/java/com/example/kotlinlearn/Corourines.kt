4b8be3270c96a1985cba2a4f5c9cde823de4bf3b
sundu <androidsundu@163.com>
2021-01-15 19:58:32 +0800

    kotlin 协程

--- kotlinlearn/src/main/java/com/example/kotlinlearn/Corourines.kt
+++ kotlinlearn/src/main/java/com/example/kotlinlearn/Corourines.kt
@@ -4,17 +4,75 @@ import kotlinx.coroutines.*
 import java.lang.Thread.sleep
 
 fun main() {
-    println("主线程 开始 id = "+Thread.currentThread().id)
-    test1()
-    println("主线程 结束 id = "+Thread.currentThread().id)
-    sleep(1000000L)
+//    GlobalScopeTest().runTest()
+//    RunBlockingTest().runTest()
+//    RunBlockingTest().runTest2()
+
 }
 
-private fun test1(){
-    GlobalScope.launch (Dispatchers.IO){
-        repeat(5){
-            delay(1000L)
-            println("协程 name = "+Thread.currentThread().name +" id = "+Thread.currentThread().id)
+
+class GlobalScopeTest{
+    /**
+     * 不会阻塞主线程
+     */
+    fun runTest(){
+        println("主线程 开始 id = "+Thread.currentThread().id)
+        test()
+        println("主线程 结束 id = "+Thread.currentThread().id)
+        sleep(1000000L)
+    }
+    private fun test(){
+        GlobalScope.launch (Dispatchers.IO){
+            repeat(5){
+                delay(1000L)
+                println("协程 name = "+Thread.currentThread().name +" id = "+Thread.currentThread().id)
+            }
+        }
+    }
+}
+
+
+class RunBlockingTest{
+
+    /**
+     *  阻塞主线程  使用当前线程
+     */
+    fun runTest(){
+        println("主线程 开始 id = "+Thread.currentThread().id)
+        test()
+        println("主线程 结束 id = "+Thread.currentThread().id)
+        sleep(1000000L)
+    }
+
+    private fun test(){
+        runBlocking{
+            launch {
+                repeat(5){
+                    delay(1000L)
+                    println("协程 name = "+Thread.currentThread().name +" id = "+Thread.currentThread().id)
+                }
+            }
+        }
+    }
+
+    /**
+     *  阻塞主线程  使用 Default 线程 或者 main 线程  可以自由切换
+     */
+    fun runTest2(){
+        println("主线程 开始 id = "+Thread.currentThread().id)
+        test2()
+        println("主线程 结束 id = "+Thread.currentThread().id)
+        sleep(1000000L)
+    }
+
+    private fun test2(){
+        runBlocking{
+            launch ( context = Dispatchers.Default ,start = CoroutineStart.DEFAULT){
+                repeat(5){
+                    delay(1000L)
+                    println("协程 name = "+Thread.currentThread().name +" id = "+Thread.currentThread().id)
+                }
+            }
         }
     }
 }
