a7634dd628bc76d1a4d7a00cd678bd7c1a5c0863
sundu <sundu@xiaomi.com>
2021-06-09 23:18:36 +0800

    添加气泡自定义View

--- app/src/main/java/com/example/demo/sundu/custview/beziercurve/dragpop/BezierCurverDragPopActivity.kt
+++ app/src/main/java/com/example/demo/sundu/custview/beziercurve/dragpop/BezierCurverDragPopActivity.kt
@@ -1,12 +1,37 @@
 package com.example.demo.sundu.custview.beziercurve.dragpop
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.demo.R
+import kotlinx.android.synthetic.main.activity_berzier_curver_drag_pop.*
 
-class BezierCurverDragPopActivity : AppCompatActivity() {
+class BezierCurverDragPopActivity : AppCompatActivity() , DragBubbleView.OnBubbleStateListener ,
+    View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_berzier_curver_drag_pop)
+        reCreateBtn.setOnClickListener(this)
+        dragBubbleView.setText("99+")
+        dragBubbleView.setOnBubbleStateListener(this)
+    }
+
+
+
+    override fun onDrag() {
+    }
+
+    override fun onRestore() {
+    }
+
+    override fun onDismiss() {
+    }
+
+    override fun onMove() {
+    }
+
+    override fun onClick(v: View?) {
+        dragBubbleView.reCreate()
     }
 }
