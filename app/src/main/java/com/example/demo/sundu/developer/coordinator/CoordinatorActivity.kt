bbf20d33d70fdeac596ed315bbb6c77c62bf506f
sundu <androidsundu@gmail.com>
2023-09-19 19:21:36 +0800

    coodinator 使用
    Signed-off-by: sundu <androidsundu@gmail.com>

--- app/src/main/java/com/example/demo/sundu/developer/coordinator/CoordinatorActivity.kt
+++ app/src/main/java/com/example/demo/sundu/developer/coordinator/CoordinatorActivity.kt
@@ -1,8 +1,13 @@
 package com.example.demo.sundu.developer.coordinator
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.demo.databinding.ActivityCoordinatorLayoutBinding
+import com.example.demo.utils.*
+import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
+
 
 class CoordinatorActivity : AppCompatActivity() {
 
@@ -10,7 +15,38 @@ class CoordinatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        immersiveStatusBar() // 沉浸式状态栏
+        immersiveNavigationBar() // 沉浸式导航栏
+        setStatusBarColor(Color.TRANSPARENT) // 设置状态栏背景色
+        setNavigationBarColor(Color.TRANSPARENT) // 设置导航栏背景色
+        setLightStatusBar(true) // 设置浅色状态栏背景（文字为深色）
+        setLightNavigationBar(true) // 设置浅色导航栏背景（文字为深色）
         viewDataBinding = ActivityCoordinatorLayoutBinding.inflate(layoutInflater)
         setContentView(viewDataBinding.root)
+
+        var slideMaxHeight = 200f
+
+        viewDataBinding.appBar.addOnOffsetChangedListener(OnOffsetChangedListener { v, verticalOffset ->
+            Log.e("sundu", "app bar - v $verticalOffset")
+            if (verticalOffset < 0) {
+                setLightStatusBar(true)
+                val slideHeight: Int = kotlin.math.abs(verticalOffset)
+                if (slideHeight < slideMaxHeight) {
+                    val proportion = ((slideHeight / slideMaxHeight) * 255).toInt()
+                    val color = Color.argb(proportion, 255, 0, 0)
+                    viewDataBinding.toolbar.setBackgroundColor(color)
+                } else {
+                    viewDataBinding.toolbar.setBackgroundColor(Color.RED)
+                }
+                viewDataBinding.tevTitle.setTextColor(Color.parseColor("#99000000"))
+                viewDataBinding.imvBack.setColorFilter(Color.parseColor("#99000000"))
+            } else {
+                setLightStatusBar(true)
+                viewDataBinding.toolbar.setBackgroundColor(Color.TRANSPARENT)
+                viewDataBinding.tevTitle.setTextColor(Color.parseColor("#FF00ff00"))
+                viewDataBinding.imvBack.setColorFilter(Color.parseColor("#FF00ff00"))
+            }
+        })
+
     }
 }
